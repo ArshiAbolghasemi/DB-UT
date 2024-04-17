71b0d68dc5c1c90e6672b3c8b2a01047890854f8
a.abolghasemi <arshiabolghasemi@gmail.com>
2024-04-17 18:50:19 +0330

    fix query nnumbers

--- HW4/db/queries.sql
+++ HW4/db/queries.sql
@@ -14,14 +14,14 @@ GROUP BY o.staff_id
 ORDER BY COUNT(o.order_id) DESC 
 LIMIT 3;
 
--- Q#4
+-- Q#3
 SELECT o.* 
 FROM orders o 
 JOIN customers c 
 	ON o.customer_id = c.customer_id 
 WHERE o.order_status = 'Processing' OR c.first_name LIKE 'A%';
 
--- Q#5
+-- Q#4
 SELECT product_id, 
 	   product_name, 
 	   list_price
